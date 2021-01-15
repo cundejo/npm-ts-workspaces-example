d91b8c6944b0c2315d012e279c5c67128b12284b
Oliver Sosa <cundejo85@gmail.com>
2021-01-15 13:05:30 -0300

    testing actions

--- packages/x-cli/src/main.ts
+++ packages/x-cli/src/main.ts
@@ -1,6 +1,8 @@
 import { awesomeFn } from "@quramy/x-core";
 
 export async function main() {
+  console.log('calling main');
+  
   // dependencies across child packages
   const out = await awesomeFn();
   return out;
